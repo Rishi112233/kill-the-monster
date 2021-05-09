66d4b24304c22562966608780aef179766ee0654
Rishi112233 <78799646+Rishi112233@users.noreply.github.com>
2021-05-09 21:50:13 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,31 @@
+class Monster{
+    constructor(x,y,width,height){
+        var options= {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.75,
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world, this.body);
+        this.image=loadImage("orange-mob.jpg");
+       
+    }
+    display(){
+        var pos  = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        // rotate(angle);
+        // rectMode(CENTER);
+        // strokeWeight(4);
+        // stroke("black");
+        // fill("blue");
+        // rect(0,0,this.width,this.height);
+        pop();
+        
+    }
+}
